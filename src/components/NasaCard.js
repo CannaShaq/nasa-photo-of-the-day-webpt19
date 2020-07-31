f87f255e7ffeed0e46f6f2211e5d55088151d97c
CannaShaq <itscoldinwinter2@gmail.com>
2020-07-30 23:35:25 -0700

    mvp
    Project Complete.

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,18 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 
 
 const NasaCard = props => {
+
+    const Title = styled.p`
+        font-family: "Lucida Console";
+        font-size: 2rem;
+        color: goldenrod;
+        text-align: center;
+        text-decoration: underline;
+    `;
+
+    const Date = styled.p`
+        font-size: .7rem;
+        text-align: right;
+    `;
+
+    const Desc = styled.p`
+       
+    `;
+
+    const DescWell = styled.div`
+        background-color: rgb(26, 102, 255);
+        border-style: groove;
+        border-color: rgb(0, 0, 102);
+        border-radius: 8px;
+    `;
 
 
 
     return (
         <div>
-            <h1>Title: {props.title}</h1>
-            <h4>Date: {props.date}</h4>
+            <Title>Title: {props.title}</Title>
             <hr></hr>
             <img src={props.url}/>
-            <p>Description: {props.explanation}</p>
-            <p>{props.copyright}</p>
+            <DescWell>
+                <h2>Description</h2>
+                <p>{props.explanation}</p> 
+                <p>{props.copyright}</p> <Date>Date: {props.date}</Date>
+            </DescWell>
         </div>
       );
 
